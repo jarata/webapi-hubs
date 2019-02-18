ff0547aeb280938ab21c10c63fb60036f7c87146
J A <jarata@gmail.com>
2019-02-18 10:38:19 -0800

    added C.R.U.D.

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const express = require('express');
+const database = require('./data/db.js');
+
 const server = express();
+
+// middleware
+server.use(express.json());
 
 server.get('/', (req, res) => {
     res.send('Hello world')
@@ -8,7 +13,80 @@ server.get('/', (req, res) => {
 server.get('/now', (req, res) => {
     const time = new Date().toLocaleTimeString();
     res.send(time)
-})
+});
+// C.R.U.D.
+
+// R. read
+server.get('/hubs', (req, res) => {
+    const hub = database.hubs
+    .find()
+        .then(hubs => {
+            res.status(200).json(hubs);
+        })
+        .catch((code, message) => {
+            res.status(200).json({
+                success: false,
+                message,
+            })
+        })
+});
+
+// C. create
+server.post('/hubs', (req, res) => {
+   const hubInfo = req.body;
+    database.hubs
+        .add(hubInfo)
+        .then(hub => {
+            res.status(201).json({ success: true, hub });
+        })
+        .catch(({ code, message }) => {
+            res.status(200).json({
+                success: false,
+                message,
+            })
+        })
+});
+
+// U. update
+
+server.put('/hubs/:id', (req, res) => {
+    const {id} = req.params;
+    const changes = req.body;
+    database.hubs
+        .update(id, changes)
+        .then(updated => {
+            if (updated) {
+                res.status(200).json({ success: true, updated })
+            } else {
+                res.status(404).json({
+                    success: false,
+                    message: "Cannot fond that id"
+                })
+            }
+        })
+        .catch(({ code, message }) => {
+            res.status(code).json({
+                success: false,
+                message,
+            })
+        })
+});
+
+// D. delete
+server.delete('/hubs/:id', (req, res) => {
+    const id = req.params.id;
+    database.hubs
+        .remove(id)
+        .then(deleted => {
+            res.status(204).end()
+        })
+        .catch(({ code, message }) => {
+            res.status(200).json({
+                success: false,
+                message,
+            })
+        })
+});
 
 server.listen(4000, () =>
     console.log('Server running on http://localhost:4000')
